304db5b11fac937e4d8c5eec7d7d91cca285d9cf
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-06-05 17:23:23 +0200

    add mock axios

---
+++ __test__/data.test.ts
@@ -0,0 +1,33 @@
+import { fetch } from "../src/data";
+
+// describe("No mock", function () {
+//     it("returns name successfully", async () => {
+//         const actual = await fetch();
+//         expect(actual.data.name).toBe("CRS-22 & IROSA");
+//     });
+// });
+
+import axios, { AxiosResponse } from "axios";
+jest.mock("axios");
+
+describe("Mock", function () {
+    beforeAll(async () => {
+        const mockedAxios = axios as jest.Mocked<typeof axios>;
+        const mockedResponse: AxiosResponse = {
+            data: {
+                name: "Henry",
+            },
+            status: 200,
+            statusText: "OK",
+            headers: {},
+            config: {},
+        };
+        mockedAxios.get.mockResolvedValue(mockedResponse);
+    });
+    it("returns name successfully", async () => {
+        expect(axios.get).not.toHaveBeenCalled();
+        const actual = await fetch();
+        expect(actual.data.name).toBe("Henry");
+        expect(axios.get).toHaveBeenCalled();
+    });
+});
